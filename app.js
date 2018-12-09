b35185519884dd6ed3d1b05703f3be51bd1ff7ba
Tamas Kuti <cooty13@gmail.com>
2018-12-09 20:38:13 +0100

    fix env file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: './variables.env' });
-const Content = require('./content');
+require('dotenv').config({ path: 'variables.env' });
+// const Content = require('./content');
 
 const app = express();
 
 app.disable('x-powered-by');
 
 app.get('/content', (req, res) => {
-  return res.json(Content);
+  return res.json({it: 'works'});
 });
 
 console.log(process.env.PORT);
