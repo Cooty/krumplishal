5be17265d19eddc91528874294d098061ec4abfd
Tamas Kuti <tamas.kuti@oetkerdigital.com>
2019-09-27 12:20:16 +0200

    Add method to documentation

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,12 +1,14 @@
 exports.index= (req, res) => {
     return res.json({
-        gifs: '/gif/list',
-        gifById: '/gif/:id',
-        music: '/music/:lang/list',
-        musicById: '/music/:lang/:id',
-        quotes: '/quote/:lang/list',
-        quoteById: '/quote/:lang/:id',
-        soundfx: '/soundfx/:lang/list',
-        soundfxById: '/soundfx/:lang/:id',
+        get: {
+            gifs: '/gif/list',
+            gifById: '/gif/:id',
+            music: '/music/:lang/list',
+            musicById: '/music/:lang/:id',
+            quotes: '/quote/:lang/list',
+            quoteById: '/quote/:lang/:id',
+            soundfx: '/soundfx/:lang/list',
+            soundfxById: '/soundfx/:lang/:id',
+        }
     });
 }
